c052316cbb0601ff23fbd68d40b9c01d60dd78a9
Christoffer Holmgard <holmgard@gmail.com>
2017-11-03 00:56:06 +0100

    First commit in new repository

---
+++ www/envisageanalytics/envisageanalytics/__init__.py
@@ -0,0 +1,26 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, request, redirect, url_for, send_from_directory
+
+app = Flask(__name__, static_url_path='/var/www/envisageanalytics/envisageanalytics/')
+
+@app.route("/")
+def root():
+    getParams = request.args
+    app.logger.info('Got parameters from GET', getParams)
+    return app.send_static_file('Analytics-Visualizations/analytics.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    getParams = request.args
+    app.logger.info('Got parameters from GET', getParams)
+  # send_static_file will guess the correct MIME type
+    return app.send_static_file(path)
+
+############### END WSGI WRAPPER ##############
+
+if __name__ == "__main__":
+    # handler = RotatingFileHandler('envisageFlask.log', maxBytes=10000, backupCount=1)
+    # handler.setLevel(logging.INFO)
+    # app.logger.addHandler(handler)
+    app.run()

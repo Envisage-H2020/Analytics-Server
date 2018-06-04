90203b96b3981438c070ee17143d4818976632a1
Daniel Mercieca <arakanis@gmail.com>
2018-06-04 13:51:59 +0200

    Redacted folder
    Redacted an unneccessary folder.

--- Backup/WindLabs3D/generate_html.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def GenerateHTML():
-    rCall = "R CMD combined_clustering_and_html_generation.R"
-    rProcess = subprocess.Popen(rCall.split(), stdout=subprocess.PIPE)
-    rOutput, rError = rProcess.communicate()
-
-GenerateHTML()
